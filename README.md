118311164c5f1dc73df72e9542f7bddc4cd9190b
FaseehFS <faseeh1080@gmail.com>
2026-02-23 19:23:34 +0530

    Add comments

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 This is a minimal Express application which demonstrates user authentication. It uses MySQL.
 
+## Convensions
+
+### General
+
+- Max line length: 80 characters.
+
+### API
+
+- Accepts and returns JSON most times.
+- If the process was successful, returns a JSON containing `message`.
+- If the process fails, returns a JSON containing `error`.
+
 ## Notes
 
-- You should install `mysql2`, not `mysql`. Run `npm install mysql2`.
+- You should install `mysql2`, not `mysql`. Run `npm install mysql2`. Click [here](https://sidorares.github.io/node-mysql2/docs) to view the docs.
--- server/app.js
+++ server/app.js
@@ -1,19 +1,39 @@
+/*
+This is the entry point of our application.
+To host the website, run this file.
+*/
+
+// Importing modules:
+
 const express = require("express");
 const path = require("path");
+
+// You can import modules either by require (CommonJS) or import (ES6).
+// ES6 modules are the modern approach. However, here we use CommonJS.
+// CommonJS modules are loaded synchronusly, while ES6 modules are loaded
+// asynchronously.
 
 const app = express();
 const PORT = 3000;
 
-// Middleware
-app.use(express.json());
+// By convension, constants are named using the SCREAMING_SNAKE_CASE.
 
-// Static frontend
+// Middleware:
+
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Routes
+// The above middleware functions has no mount path, meaning they will execute
+// every time the app recieves a request.
+// However, the below middleware has a mount path.
+
 app.use("/api", require("./routes"));
 
-// Start server
+// This mounts the router (from ./routes/index.js) on the app.
+
+// See https://expressjs.com/en/guide/using-middleware.html to learn more about
+// middleware functions.
+
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
