3c9aad035985dd163ac039adbab1edf578c3b723
FaseehFS <faseeh1080@gmail.com>
2026-03-05 11:33:21 +0530

    Restrict user/delete route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const userController = require("../controllers/userController");
 const restrict = require("../middleware/restrict");
 
-router.get("/all", userController.getAllUsers);
+router.get("/all", restrict, userController.getAllUsers);
 router.get("/details", restrict, userController.getUserDetails);
 router.get("/is-logged-in", userController.isLoggedIn);
 router.get("/logout", userController.logout);
 router.post("/new", userController.createNewUser);
 router.post("/login", userController.login);
-router.post("/delete", userController.deleteUser);
+router.post("/delete", restrict, userController.deleteUser);
 
 module.exports = router;
