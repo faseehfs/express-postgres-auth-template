fe5c130139371fcb64ba289fe1ea12e048416d29
FaseehFS <faseeh1080@gmail.com>
2026-03-06 11:55:39 +0530

    Update login page

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,4 +1,5 @@
 function errorHandler(err, req, res, next) {
+  console.log("This message is printed by errorHandler.js:\n", err);
   return res.status(500).json({ error: "Internal Server Error" });
 }
 
